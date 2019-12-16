ecb34284e791379f14446b06d5c99106eb3eaca2
Dennis Akpenyi <dennisakpenyi@gmail.com>
2019-12-16 01:17:00 +0100

    Safe Args feature from gradle plugin added. GameFragment.kt, GameOverFragment.kt, GameWonFragment.kt, TitleFragment.kt, navigation.xml files updated.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -20,7 +20,7 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
         //The complete onClickListener with Navigation using createNavigateOnClickListener
-        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        binding.playButton.setOnClickListener {v: View -> v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())}
         setHasOptionsMenu(true)
         return binding.root
 
