6f52d94905fce1cbdc640c876f0166eed462ce3e
Dennis Akpenyi <dennisakpenyi@gmail.com>
2019-12-17 03:05:10 +0100

    Code updated to use ShareCompat IntentBuilder API to support content sharing from the app using Implicit Intent.

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -45,19 +46,20 @@ class GameWonFragment : Fragment() {
         inflater.inflate(R.menu.winner_menu, menu)
     }
 
-    private fun getShareIntent() : Intent {
+    private fun getShareIntent(): Intent {
         var args = fromBundle(arguments!!)
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
-        return shareIntent
+        return ShareCompat.IntentBuilder.from(activity)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
     }
 
-    private fun shareSuccess(){
+    private fun shareSuccess() {
         startActivity(getShareIntent())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
+        when (item.itemId) {
             R.id.share -> shareSuccess()
         }
         return super.onOptionsItemSelected(item)
